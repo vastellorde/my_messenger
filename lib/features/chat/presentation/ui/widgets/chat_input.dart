4fc26cd82a2fb90b4fb36a9ae17382de189a48b8
Arman <34960835+vastellorde@users.noreply.github.com>
2023-12-21 21:46:09 +0600

    feat(chat): add emoji picker

--- lib/features/chat/presentation/ui/widgets/chat_input.dart
+++ lib/features/chat/presentation/ui/widgets/chat_input.dart
@@ -1,5 +1,7 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:my_messenger/app/theme/app_colors.dart';
@@ -15,6 +17,7 @@ class ChatInput extends StatefulWidget {
 
 class _ChatInputState extends State<ChatInput> {
   final controller = TextEditingController();
+  final _emojiShowed = ValueNotifier(false);
 
   @override
   void dispose() {
@@ -22,46 +25,106 @@ class _ChatInputState extends State<ChatInput> {
     super.dispose();
   }
 
+  void _onBackspacePressed() {
+    controller
+      ..text = controller.text.characters.toString()
+      ..selection = TextSelection.fromPosition(
+          TextPosition(offset: controller.text.length));
+  }
+
   @override
   Widget build(BuildContext context) {
     final userId = FirebaseAuth.instance.currentUser?.uid ?? '';
     final roomId = context.routeData.pathParams.getString('roomId');
     return Container(
-      height: 72,
       decoration: const BoxDecoration(color: Colors.white),
       padding: const EdgeInsets.symmetric(vertical: 14, horizontal: 16),
       width: MediaQuery.sizeOf(context).width,
-      child: TextFormField(
-        controller: controller,
-        decoration: InputDecoration(
-          hintText: 'Type your message...',
-          suffixIcon: IconButton(
-            onPressed: () {
-              context.read<ChatBloc>().add(
-                    ChatEvent.sendMessage(
-                      SendMessageParams(
-                        text: controller.text,
-                        senderId: userId,
-                        receiverId: userId,
-                        roomId: roomId,
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          TextFormField(
+            controller: controller,
+            decoration: InputDecoration(
+              hintText: 'Type your message...',
+              suffixIcon: IconButton(
+                onPressed: () {
+                  context.read<ChatBloc>().add(
+                        ChatEvent.sendMessage(
+                          SendMessageParams(
+                            text: controller.text,
+                            senderId: userId,
+                            receiverId: userId,
+                            roomId: roomId,
+                          ),
+                        ),
+                      );
+                  controller.clear();
+                },
+                icon: const Icon(
+                  Icons.send,
+                  color: AppColors.gray,
+                ),
+              ),
+              prefixIcon: IconButton(
+                onPressed: () {
+                  _emojiShowed.value = !_emojiShowed.value;
+                  FocusScope.of(context).unfocus();
+                },
+                icon: const Icon(
+                  Icons.add,
+                  color: AppColors.gray,
+                ),
+              ),
+            ),
+          ),
+          ValueListenableBuilder(
+              valueListenable: _emojiShowed,
+              builder: (context, showing, _) {
+                return Offstage(
+                  offstage: !showing,
+                  child: SizedBox(
+                    height: 250,
+                    child: EmojiPicker(
+                      textEditingController: controller,
+                      onBackspacePressed: _onBackspacePressed,
+                      config: Config(
+                        columns: 7,
+                        // Issue: https://github.com/flutter/flutter/issues/28894
+                        emojiSizeMax: 32 *
+                            (defaultTargetPlatform == TargetPlatform.iOS
+                                ? 1.30
+                                : 1.0),
+                        verticalSpacing: 0,
+                        horizontalSpacing: 0,
+                        gridPadding: EdgeInsets.zero,
+                        bgColor: Colors.white,
+                        indicatorColor: Colors.blue,
+                        iconColor: Colors.grey,
+                        iconColorSelected: Colors.blue,
+                        backspaceColor: Colors.blue,
+                        skinToneDialogBgColor: Colors.white,
+                        skinToneIndicatorColor: Colors.grey,
+                        enableSkinTones: true,
+                        recentTabBehavior: RecentTabBehavior.RECENT,
+                        recentsLimit: 28,
+                        replaceEmojiOnLimitExceed: false,
+                        noRecents: const Text(
+                          'No Recents',
+                          style: TextStyle(fontSize: 20, color: Colors.black26),
+                          textAlign: TextAlign.center,
+                        ),
+                        loadingIndicator: const SizedBox.shrink(),
+                        tabIndicatorAnimDuration: kTabScrollDuration,
+                        categoryIcons: const CategoryIcons(),
+                        buttonMode: ButtonMode.MATERIAL,
+                        checkPlatformCompatibility: true,
                       ),
                     ),
-                  );
-              controller.clear();
-            },
-            icon: const Icon(
-              Icons.send,
-              color: AppColors.gray,
-            ),
-          ),
-          prefixIcon: IconButton(
-            onPressed: () {},
-            icon: const Icon(
-              Icons.add,
-              color: AppColors.gray,
-            ),
-          ),
-        ),
+                  ),
+                );
+              })
+        ],
       ),
     );
   }
